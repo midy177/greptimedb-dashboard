ee821c5885054f2087c5a45638522d33cad67ffe
wuly <wuly@greptime.com>
2026-05-26 17:16:17 +0800

    revert: restore page-layout keep-alive to fix blank page on route switch

--- src/layout/page-layout.vue
+++ src/layout/page-layout.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
 router-view(v-slot="{ Component }")
   transition(name="fade" mode="out-in" appear)
-    component(:key="route.fullPath" :is="Component" :database="appStore.database")
+    component(v-if="route.meta.ignoreCache" :is="Component" :database="appStore.database")
+    keep-alive(v-else)
+      component(:is="Component" :database="appStore.database")
 </template>
 
 <script lang="ts" setup name="PageLayout">
